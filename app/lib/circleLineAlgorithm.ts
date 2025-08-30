ebb601f6d3539a0994f49d454fe3c3ee15fd1df0
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-08-30 20:51:54 +0200

    feat: animated logo

---
+++ app/lib/circleLineAlgorithm.ts
@@ -0,0 +1,103 @@
+const translateVector = (vector: { x: number; y: number }, translation: { x: number; y: number }) => ({
+  x: vector.x + translation.x,
+  y: vector.y + translation.y
+});
+
+const rotateVector = (vector: { x: number; y: number }, angle: number) => ({
+  x: vector.x * Math.cos(angle) - vector.y * Math.sin(angle),
+  y: vector.x * Math.sin(angle) + vector.y * Math.cos(angle)
+});
+
+const invertVector = (vector: { x: number; y: number }) => ({
+  x: -vector.x,
+  y: -vector.y
+});
+
+const scaleVector = (vector: { x: number; y: number }, scale: number) => ({
+  x: vector.x * scale,
+  y: vector.y * scale
+});
+
+const rotateVectorAroundCenter = (vector: { x: number; y: number }, center: { x: number; y: number }, angle: number) =>
+  translateVector(rotateVector(translateVector(vector, invertVector(center)), angle), center);
+
+export const circleAtIndex = (radius: number, knots: number, index: number) => {
+  const angleSegment = (Math.PI * 2) / knots;
+  const arcDistance = radius * angleSegment;
+
+  const cDelta = {
+    x: index * arcDistance,
+    y: 0
+  };
+
+  const dCenter = { x: 0, y: radius };
+
+  const pointDirectionArray: {
+    dBefore: { x: number; y: number };
+    dAfter: { x: number; y: number };
+    position: { x: number; y: number };
+  }[] = [];
+
+  const dBefore = { x: -arcDistance * 0.25, y: 0 };
+  const dAfter = { x: arcDistance * 0.25, y: 0 };
+
+  for (let i = 0; i < knots + 1; i++) {
+    const position = {
+      x: i * arcDistance,
+      y: 0
+    };
+    if (i <= index) pointDirectionArray.push({ dBefore, dAfter, position });
+    else {
+      const angle = (i - index) * angleSegment;
+      pointDirectionArray.push({
+        dBefore: rotateVectorAroundCenter(dBefore, { x: 0, y: 0 }, angle),
+        dAfter: rotateVectorAroundCenter(dAfter, { x: 0, y: 0 }, angle),
+        position: translateVector(rotateVectorAroundCenter({ x: 0, y: 0 }, dCenter, angle), cDelta)
+      });
+    }
+  }
+
+  return pointDirectionArray;
+};
+
+const vertexToString = (vertex: { x: number; y: number }) => `${vertex.x.toFixed(2)},${vertex.y.toFixed(2)}`;
+
+const getPathForPointDirectionArray = (
+  pointDirectionArray: {
+    dBefore: { x: number; y: number };
+    dAfter: { x: number; y: number };
+    position: { x: number; y: number };
+  }[]
+) => {
+  let path = `M ${vertexToString(pointDirectionArray[0].position)}`;
+  for (let i = 1; i < pointDirectionArray.length; i++) {
+    path += ` C ${vertexToString(
+      translateVector(pointDirectionArray[i - 1].position, pointDirectionArray[i - 1].dAfter)
+    )}`;
+    path += ` ${vertexToString(translateVector(pointDirectionArray[i].position, pointDirectionArray[i].dBefore))}`;
+    path += ` ${vertexToString(pointDirectionArray[i].position)}`;
+  }
+  return path;
+};
+
+export const getPathArrayForRadiusAndKnots = (radius: number, knots: number) => {
+  const pathArray: string[] = [];
+
+  for (let i = 0; i < knots + 1; i++) {
+    pathArray.push(getPathForPointDirectionArray(circleAtIndex(radius, knots, i)));
+  }
+
+  // add the first a couple of times add the last a couple of times and then add an inverted instance of the whole pathArray
+  pathArray.push(...pathArray.slice(-1));
+  pathArray.push(...pathArray.slice(-1));
+  pathArray.push(...pathArray.slice(-1));
+  pathArray.push(...pathArray.slice().reverse());
+  pathArray.unshift(...pathArray.slice(0));
+  pathArray.unshift(...pathArray.slice(0));
+  pathArray.unshift(...pathArray.slice(0));
+  pathArray.unshift(...pathArray.slice(0));
+  pathArray.unshift(...pathArray.slice(0));
+  pathArray.unshift(...pathArray.slice(0));
+
+  return pathArray.join(';\n');
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import { getPathArrayForRadiusAndKnots } from './lib/circleLineAlgorithm';
+
 export default function Home() {
   return (
-    <main>
-      <div>Next.js on GitHub Pages</div>
+    <main title="circrete" className="bg-circrete-mokka">
+      <svg width="800" height="800" xmlns="http://www.w3.org/2000/svg" viewBox="-100 -100 400 300">
+        <path id="base-path" stroke="transparent" fill="transparent" strokeWidth="0">
+          <animate
+            attributeName="d"
+            dur={500}
+            repeatCount="indefinite"
+            values={getPathArrayForRadiusAndKnots(30, 45)}
+          />
+        </path>
+        <text strokeWidth="5" fontSize="50" fontWeight="bold" color="inherit">
+          <textPath href="#base-path" stroke="var(--color-circrete-red)" fill="transparent" strokeWidth={1}>
+            c
+          </textPath>
+          <textPath href="#base-path" fill="var(--color-circrete-red)">
+            &nbsp;&nbsp;&nbsp;ircrete
+          </textPath>
+        </text>
+      </svg>
     </main>
   );
 }
