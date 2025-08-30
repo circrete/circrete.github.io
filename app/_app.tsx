be1f6c8a1ba41e7222cf748fd2f296354e959e44
Jonas Ward <jonas.vandenbulcke@gmail.com>
2025-08-30 16:14:05 +0200

    feat: localization

---
+++ app/_app.tsx
@@ -0,0 +1,12 @@
+import { NextIntlClientProvider } from 'next-intl';
+import { useRouter } from 'next/router';
+
+export default function App({ Component, pageProps }: { Component: React.ComponentType; pageProps: any }) {
+  const router = useRouter();
+
+  return (
+    <NextIntlClientProvider locale={router.locale} timeZone="Europe/Vienna" messages={pageProps.messages}>
+      <Component {...pageProps} />
+    </NextIntlClientProvider>
+  );
+}
